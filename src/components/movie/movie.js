a31f59fd0f33173413bc3c1e78ce594ee6df8f09
Yekaterina_Bulekova <yekaterina_bulekova@epam.com>
2019-03-11 17:01:22 +0600

    Fixed styles in movie component

--- src/components/movie/movie.js
+++ src/components/movie/movie.js
@@ -19,7 +19,6 @@ class Movie extends PureComponent {
 
     render() {
         const {movie, onCardClick} = this.props
-        console.log(movie.date)
         return (
             <div className="movie-card" onClick = {onCardClick}>
                 <div className="movie-img">
@@ -27,13 +26,12 @@ class Movie extends PureComponent {
                 </div>
                 <div className="movie-info">
                     <div className="movie-title">{movie.title}</div>
-                    <div className="movie-release-date">{(new Date(movie.release_date)).getFullYear().toString()}</div>
+                    <div className="movie-release-date"><p>{(new Date(movie.release_date)).getFullYear().toString()}</p></div>
                     <div className="movie-genres">{movie.genres.toString()}</div>
                 </div>
             </div>
         )
     }
 }
-
 
 export default Movie
