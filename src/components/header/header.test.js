59273b726a52e6119b80822ca0916ed550bd546f
Yekaterina_Bulekova <yekaterina_bulekova@epam.com>
2019-03-28 13:47:26 +0600

    added test:coverage and test:watch scripts to package.json, moved message test to its folder, added second simple test for header component

---
+++ src/components/header/header.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {act} from 'react-dom/test-utils';
+import Message, { Header } from './header';
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
+
+it('can render with name', () => {
+    var testName = "Test name of app"
+    act(() => {
+      ReactDOM.render(<Header name={testName} />, container);
+    });
+    const header = container.querySelector('.app-header .header-name');
+    expect(header.textContent).toBe(testName);
+});
